2012f512fc24c6e76b2451e81ef2ee1c3364dc04
IVS737 <salata.vanya191@gmail.com>
2023-03-03 13:33:24 +0200

    feat: absolute path configuration

--- src/js/searchform.js
+++ src/js/searchform.js
@@ -1,50 +1,49 @@
 const refs = {
-    form: document.querySelector(".header-search-form"),
-    input: document.querySelector(".header-search-input"),
-    submitButton: document.querySelector('.header-button-makesearch'),
-    openInputButton: document.querySelector('.header-button-opensearch'),
-    withoutNewsContainer: document.querySelector('.container__error'),
-    newsList: document.querySelector('.wrapper__list'),
-}
+  form: document.querySelector('.header-search-form'),
+  input: document.querySelector('.header-search-input'),
+  submitButton: document.querySelector('.header-button-makesearch'),
+  openInputButton: document.querySelector('.header-button-opensearch'),
+  withoutNewsContainer: document.querySelector('.container__error'),
+  newsList: document.querySelector('.wrapper__list'),
+};
 
-const { form, input, submitButton, openInputButton,  withoutNewsContainer, newsList} = refs;
+const { form, input, submitButton, openInputButton, withoutNewsContainer, newsList } = refs;
 
 const KEY = 'kAFi92vRzv66C7DQ6coSA3C5NLbSIILk';
 form.addEventListener('submit', onFormSubmit);
 openInputButton.addEventListener('click', onOpenInputButtonClick);
 
-
-
-let value = "";
+let value = '';
 
 function onFormSubmit(event) {
-    value = event.currentTarget.elements.newsField.value.trim();
-    event.preventDefault();
+  value = event.currentTarget.elements.newsField.value.trim();
+  event.preventDefault();
 
-    fetchNews(value)
-        .then(data => {
-            if (data.response.docs.length === 0) {
-                form.reset();
-                return createEmptyMarkup();
-            }
-            makeMarkup(data.response.docs);
-            return (data.response.docs);
-        }
-        ).catch(onError);
-  
+  fetchNews(value)
+    .then((data) => {
+      if (data.response.docs.length === 0) {
+        form.reset();
+        return createEmptyMarkup();
+      }
+      makeMarkup(data.response.docs);
+      return data.response.docs;
+    })
+    .catch(onError);
 }
 
 function fetchNews(value) {
-  return fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${value}&api-key=${KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${value}&api-key=${KEY}`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    },
+  );
 }
 
 function createEmptyMarkup() {
-    const emptyMarkup = `<h2 class="withoutnews-title">We haven’t found news from <br> this category</h2><picture>
+  const emptyMarkup = `<h2 class="withoutnews-title">We haven’t found news from <br> this category</h2><picture>
                   <source
                     media="(min-width:1280px)"
                     srcset="
@@ -71,31 +70,31 @@ function createEmptyMarkup() {
                     loading="lazy"
                     class="withoutnews-image"
                   />
-                </picture>`
-    withoutNewsContainer.innerHTML = emptyMarkup;
-    
+                </picture>`;
+  withoutNewsContainer.innerHTML = emptyMarkup;
 }
 
 function makeMarkup(array) {
-    const markUp = array.map(data => {
-    const subTitle = data.abstract.slice(0, 100) + `...`;
-    const title = data.headline.main.slice(0, 60) + `...`;
-    const date = data.pub_date.toString().slice(0, 10).replace(`-`, '/').replace(`-`, '/');
+  const markUp = array
+    .map((data) => {
+      const subTitle = data.abstract.slice(0, 100) + `...`;
+      const title = data.headline.main.slice(0, 60) + `...`;
+      const date = data.pub_date.toString().slice(0, 10).replace(`-`, '/').replace(`-`, '/');
 
-    let imageAdress;
-    let imageStartAdress;
+      let imageAddress;
+      let imageStartAddress;
 
-    if (data.multimedia.length === 0) {
-      imageAdress = 'https://st.depositphotos.com/1000558/53737/v/1600/depositphotos_537370102-stock-illustration-image-photo-sign-symbol-template.jpg';
-    }
-    else if (data.multimedia.length > 0) {
-      imageStartAdress = 'https://static01.nyt.com/';
-      imageAdress = imageStartAdress + data.multimedia[0].url;
-    }
-    return `<li class = "card-item" data-id = "${data.title}">
+      if (data.multimedia.length === 0) {
+        imageAddress =
+          'https://st.depositphotos.com/1000558/53737/v/1600/depositphotos_537370102-stock-illustration-image-photo-sign-symbol-template.jpg';
+      } else if (data.multimedia.length > 0) {
+        imageStartAddress = 'https://static01.nyt.com/';
+        imageAddress = imageStartAddress + data.multimedia[0].url;
+      }
+      return `<li class = "card-item" data-id = "${data.title}">
     <div class="card-wrapper">
       <div class="card-thumb">
-        <img class="card-image" src = "${imageAdress}" alt = "${data.byline}">
+        <img class="card-image" src = "${imageAddress}" alt = "${data.byline}">
         <p class="card-news-category">${data.section_name}</p>
         <p class="card-text-read">Already read</p>
         <button class="favorite-button" type="button" data-action="favorite-button">Add to favorite
@@ -109,18 +108,18 @@ function makeMarkup(array) {
       </div>
     </div>
 </li>`;
-  }).join('');
+    })
+    .join('');
 
-    newsList.innerHTML = markUp;
+  newsList.innerHTML = markUp;
 }
 
-
 function onError(error) {
-    createEmptyMarkup()
-    console.log(error);
+  createEmptyMarkup();
+  console.log(error);
 }
 
 function onOpenInputButtonClick(event) {
-    openInputButton.style.display = "none";
-    form.style.display = "block";
+  openInputButton.style.display = 'none';
+  form.style.display = 'block';
 }
