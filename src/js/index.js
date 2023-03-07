a375be895c0a4a09dedb89f3f5e825c18abdb60a
IVS737 <salata.vanya191@gmail.com>
2023-03-08 01:11:19 +0200

    fix: indexjs

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import header from './header';
 import searchform from './searchform';
+import weather from './weather';
 import themeChange from './themeChange';
 import newslistfiltering from './newslistfiltering';
 import './CategoryList/categoryList';
 import filteredPagination from './filteredPagination';
 import searchPagination from './searchPagination';
-import weather from './weather';
