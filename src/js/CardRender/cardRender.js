f83fcbd773f27eaf9b8aff3df43085e45353a9ae
IVS737 <salata.vanya191@gmail.com>
2023-03-07 15:50:42 +0200

    fix: card render

--- src/js/CardRender/cardRender.js
+++ src/js/CardRender/cardRender.js
@@ -30,24 +30,26 @@ export default function Makemarkup(array) {
         }
       }
 
-      return `<li class = "card-item" data-id = "${data.title}">
-      <div class="card-wrapper">
-        <div class="card-thumb">
-          <img class="card-image" src = "${imageAddress}" alt = "${data.byline}">
-          <p class="card-news-category">${data.section_name}</p>
-          <p class="card-text-read">Already read
-          <svg width="18" height="18" class="check-icon"><use href="../images/symbol-defs.svg#icon-check"</svg></p>
-          <button class="favourite-button" type="button" data-action="favourite-button">Add to favorite
-          <svg class="add-to-favourite"width ="16" height="16"><use href="../images/symbol-defs.svg#icon-heart"></use><svg></button>
-        </div>
-        <h3 class="card-news-title">${data.headline.main}</h3>
-        <p class="card-news-description">${subTitle}</p>
-        <div class="card-info-container">
-          <p class="card-datetime">${date}</p>
-          <a class="card-link" href="${data.web_url}" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
-        </div>
+      return `<li class = "card-item" data-id = "${data.headline.main}">
+
+    <div class="card-wrapper">
+      <div class="card-thumb">
+        <img class="card-image" src = "${imageAddress}" alt = "${data.byline}">
+        <p class="card-news-category">${data.section_name}</p>
+
+        <p class="card-text-read">Already read
+        <svg width="18" height="18" class="check-icon"><use href="../images/symbol-defs.svg#icon-check"</svg></p>
+        <button class="favourite-button" type="button" data-action="favourite-button">Add to favorite</button>
+
       </div>
-  </li>`;
+      <h3 class="card-news-title">${data.headline.main}</h3>
+      <p class="card-news-description">${subTitle}</p>
+      <div class="card-info-container">
+        <p class="card-datetime">${date}</p>
+        <a class="card-link" href="${data.web_url}" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
+      </div>
+    </div>
+</li>`;
     })
     .join('');
 
--- src/js/CardRender/cardRenderStart.js
+++ src/js/CardRender/cardRenderStart.js
@@ -25,24 +25,26 @@ export default function MakeStartMarkup(array) {
         imageStartAddress = 'https://static01.nyt.com/';
         imageAddress = data.multimedia[1].url;
       }
-      return `<li class = "card-item" data-id = "${data.title}">
-      <div class="card-wrapper">
-        <div class="card-thumb">
-          <img class="card-image" src = "${imageAddress}" alt = "${data.byline}">
-          <p class="card-news-category">${data.section}</p>
-          <p class="card-text-read">Already read
-          <svg width="18" height="18" class="check-icon"><use href="../images/symbol-defs.svg#icon-check"</svg></p>
-          <button class="favourite-button" type="button" data-action="favourite-button">Add to favorite
-          <svg class="add-to-favourite"width ="16" height="16"><use href="../images/symbol-defs.svg#icon-heart"></use><svg></button>
-        </div>
-        <h3 class="card-news-title">${data.title}</h3>
-        <p class="card-news-description">${subTitle}</p>
-        <div class="card-info-container">
-          <p class="card-datetime">${date}</p>
-          <a class="card-link" href="${data.short_url}" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
-        </div>
+      return `<li class = "card-item" data-id = "${data.headline.main}">
+
+    <div class="card-wrapper">
+      <div class="card-thumb">
+        <img class="card-image" src = "${imageAddress}" alt = "${data.byline}">
+        <p class="card-news-category">${data.section_name}</p>
+
+        <p class="card-text-read">Already read
+        <svg width="18" height="18" class="check-icon"><use href="../images/symbol-defs.svg#icon-check"</svg></p>
+        <button class="favourite-button" type="button" data-action="favourite-button">Add to favorite</button>
+
       </div>
-  </li>`;
+      <h3 class="card-news-title">${data.headline.main}</h3>
+      <p class="card-news-description">${subTitle}</p>
+      <div class="card-info-container">
+        <p class="card-datetime">${date}</p>
+        <a class="card-link" href="${data.web_url}" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
+      </div>
+    </div>
+</li>`;
     })
     .join('');
 
