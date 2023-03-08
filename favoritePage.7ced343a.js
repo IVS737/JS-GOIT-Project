3db0a875fe5e1e5a3a7e4851abe34b85b5a57daa
IVS737 <salata.vanya191@gmail.com>
2023-03-08 19:03:49 +0000

    Deploying to gh-pages from @ IVS737/js-goit-project@6c581af6c16ea8ca5cfa5f20a2b627adfd0cbf99 🚀

---
+++ favoritePage.7ced343a.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".to-top");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){window.pageYOffset>100?o.classList.remove("is-hidden"):o.classList.add("is-hidden")}));
+//# sourceMappingURL=favoritePage.7ced343a.js.map
---
+++ favoritePage.a5375c55.js
@@ -0,0 +1,2 @@
+!function(){const o=document.querySelector(".to-top");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){window.pageYOffset>100?o.classList.remove("is-hidden"):o.classList.add("is-hidden")}))}();
+//# sourceMappingURL=favoritePage.a5375c55.js.map
